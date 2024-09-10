bd82969ffb9ea31354af6be70e121c6a728e3571
SreayesAakash <114054972+SreayesAakash@users.noreply.github.com>
2024-09-11 00:43:14 +0530

    Add files via upload

---
+++ assests/main.js
@@ -0,0 +1,7 @@
+var typed = new typed (".text", {
+    strings:["Aspiring Frontend Developer", "SQL Developer", ],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
